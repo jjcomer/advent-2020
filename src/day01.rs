1872cfd4b3ce248b51690d78cd4246b6c26e1ed4
Josh Comer <jcomer@cvent.com>
2020-12-01 06:09:21 -0400

    Solve day 1

---
+++ src/day01.rs
@@ -0,0 +1,26 @@
+use itertools::iproduct;
+
+#[aoc_generator(day1)]
+pub fn parse_input(input: &str) -> Vec<i32> {
+    input.lines().map(|s| s.parse::<i32>().unwrap()).collect()
+}
+
+#[aoc(day1, part1)]
+pub fn solve_part1(input: &Vec<i32>) -> i32 {
+    for (x, y) in iproduct!(input, input) {
+        if x != y && x + y == 2020 {
+            return x * y;
+        }
+    }
+    -1
+}
+
+#[aoc(day1, part2)]
+pub fn solve_part2(input: &Vec<i32>) -> i32 {
+    for (x, y, z) in iproduct!(input, input, input) {
+        if x != y && y != z && z != x && x + y + z == 2020 {
+            return x * y * z;
+        }
+    }
+    -1
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
 extern crate aoc_runner;
 
 #[macro_use]
+extern crate lazy_static;
+
+#[macro_use]
 extern crate aoc_runner_derive;
+
+pub mod day01;
 
 aoc_lib! {year=2020}
